a5ceaf5e52f72368bc07e510fa6e8cd999872555
Hazarath1234 <93900878+Hazarath1234@users.noreply.github.com>
2021-11-28 21:35:25 +0530

    Create area trle .c

---
+++ area trle .c
@@ -0,0 +1,10 @@
+int main()
+{
+    float base,height,area;
+    printf("enter the value of the base of tringle:");
+    printf("enter the value of the height of tringle:");
+    scanf("%f%f",&base,&height);
+    area=(0.5*base*height);
+    printf("the area of tringle is ; %f",area);
+    return 0;
+}
