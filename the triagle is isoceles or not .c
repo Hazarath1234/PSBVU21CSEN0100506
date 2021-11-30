3130512b85dc16d98ff6edbba800f24cb8167481
Hazarath1234 <93900878+Hazarath1234@users.noreply.github.com>
2021-11-30 10:44:08 +0530

    Create the triagle is isoceles or not .c

---
+++ the triagle is isoceles or not .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value of a");
+	scanf("%d",&a);
+	printf("enter the value of b");
+	scanf("%d",&b);
+	printf("enter the value of c");
+	scanf("%d",&c);
+	if((a==b)||(b==c)||(c==a))
+	{printf("the triangle is isoceles");
+	}
+	else
+	{printf("the triangle is not isoceles");
+}
+	}
+
+
