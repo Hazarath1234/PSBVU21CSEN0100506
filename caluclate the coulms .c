dc59d3944ad910d0626b0577f7c932bd81fd497a
Hazarath1234 <93900878+Hazarath1234@users.noreply.github.com>
2021-12-06 12:49:44 +0530

    Create caluclate the coulms .c

---
+++ caluclate the coulms .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int i,j,colums;
+	printf("enter the number of colums");
+	scanf("\n%d",&colums);
+	
+	{
+		for (j=1;j<=colums;j++)
+		{
+			printf("*");
+		}
+		printf("*\n");
+		}
+		
+	}
