14349933a516561052107216bf8cd86249c15b66
Hazarath1234 <93900878+Hazarath1234@users.noreply.github.com>
2021-12-28 10:38:41 +0530

    Create no argument no return .c

---
+++ no argument no return .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    sum();
+}
+void sum()
+{
+    int a,b;
+    printf("enter the two numbers");
+    scanf("%d%d",&a,&b);
+    printf("the sum is %d",a+b);
+}
