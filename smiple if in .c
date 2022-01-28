6e20561889848c25f1454aaec484097ffbac5090
Hazarath1234 <93900878+Hazarath1234@users.noreply.github.com>
2022-01-28 17:15:27 +0530

    Create smiple if in .c

---
+++ smiple if in .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int m;
+	printf("enter the number=");
+	scanf("%d",&m);
+	if(m>=0)
+	{
+	printf("the number is positive");
+	}
+	if(m<=0)
+	{
+	printf("the number is negative");
+	}
+}
